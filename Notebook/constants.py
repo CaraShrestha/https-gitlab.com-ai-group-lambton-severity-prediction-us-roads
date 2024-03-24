fce230aafcbefee4d9285d5b1efcf5f1b620333c
Mariana Rosete <vmrosete@gmail.com>
2024-03-24 17:26:48 -0400

    Modelbuilding datacleaning modification

--- Notebook/constants.py
+++ Notebook/constants.py
@@ -1,2 +1,3 @@
 dataset_url="C:/Users/Bipin/IdeaProjects/severity-prediction-us-roads/Dataset/US_Accidents_March23.csv"
 ds_ivan='C:/python/ds.csv'
+datasetmar_url="C:/Users/vmari/OneDrive/Desktop/Lambton/2nd_term/AI/Project_Accidents_severity/US_Accidents_March23.csv"
