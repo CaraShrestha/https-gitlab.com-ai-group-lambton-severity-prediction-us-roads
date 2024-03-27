18efc5df2da99bc0146f003a174dd6c7750e80bc
Daniela Salazar <dany.sfdl@gmail.com>
2024-03-27 18:25:35 -0400

    AP-29 URL CONSTANT DANIELA

---
+++ Notebook/constants.py
@@ -0,0 +1,5 @@
+dataset_url="C:/Users/Bipin/IdeaProjects/severity-prediction-us-roads/Dataset/US_Accidents_March23.csv"
+
+
+
+dataset_url_dani='C:/Users/danie/OneDrive/Documents/T2/IA/Project/US_Accidents_March23.csv'
