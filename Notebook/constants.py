f7c73cc9b8b9cddc02ab159f7cbe7e9fdcd5f4f7
ivantravisany@yahoo.com <itravisany@gmail.com>
2024-04-12 00:45:48 -0400

    last run before switching branches

--- Notebook/constants.py
+++ Notebook/constants.py
@@ -2,4 +2,4 @@ dataset_url="C:/Users/Bipin/IdeaProjects/severity-prediction-us-roads/Dataset/US
 ds_ivan='C:/python/ds.csv'
 datasetmar_url="C:/Users/vmari/OneDrive/Desktop/Lambton/2nd_term/AI/Project_Accidents_severity/US_Accidents_March23.csv"
 ds_angel="C:/Users/angel/OneDrive/Escritorio/Lambton_Angel/Classes/2_Term/Introduction_to_Artificial_Intelligence/Project/US_Accidents_March23.csv"
-ds_mac='/Users/ivantravisany/Documents/Estudios/Python/dataset'
+ds_mac='/Users/ivantravisany/Documents/Estudios/Python/dataset/US_Accidents_March23.csv'
